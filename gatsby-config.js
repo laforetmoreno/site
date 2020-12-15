f8a31e336a98508f4c3404039cf59f04490f8ec1
Claudius Ibn <ibnclaudius@gmail.com>
2020-12-15 00:15:56 -0300

    Add Facebook pixel

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,12 @@
 module.exports = {
-  plugins: [`gatsby-plugin-styled-components`],
-  plugins: [`gatsby-plugin-netlify`],
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-netlify`,
+    {
+      resolve: `gatsby-plugin-facebook-pixel`,
+      options: {
+        pixelId: `1309100156130776`,
+      },
+    },
+  ],
 };
